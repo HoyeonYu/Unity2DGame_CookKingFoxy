07242438964319389e2a63e60d63fc23a76aeb38
ghdus819 <ghdus819@gmail.com>
2021-04-25 09:08:08 +0900

    Add Roulette Scene

--- Assets/Scripts/InfoScripts/InfoTypingEffect.cs
+++ Assets/Scripts/InfoScripts/InfoTypingEffect.cs
@@ -11,14 +11,14 @@ public class InfoTypingEffect : MonoBehaviour
     void Start()
     {
         StartCoroutine(_typing());
-        infoText[0] = "나는야 요리왕 폭시!\n\n";
-        infoText[1] = "어떤 요리를 할지 <b>룰렛</b>으로 정해보자.\n";
-        infoText[2] = "룰렛은 <b>마우스</b>를 누르면 돌아가고, 마우스를 떼면 멈춰.\n\n";
-        infoText[3] = "먼저 필요한 재료를 <b>제한시간</b> 내에 모아야 돼.\n";
-        infoText[4] = "재료는 <b>하늘</b>에서 떨어지고, 몸에 닿으면 <b>인벤토리</b>에 저장돼.\n";
-        infoText[5] = "<b>쓰레기</b>에 닿으면 재료가 모두 상하니 조심하자.\n";
+        infoText[0] = "나는야 요리왕 폭시!\n";
+        infoText[1] = "어떤 요리를 할지 <b>룰렛</b>으로 정해보자.\n\n";
+        infoText[2] = "요리를 정하면 필요한 재료를 <b>제한시간 1분</b> 내에 모아야 돼.\n";
+        infoText[3] = "재료는 하늘에서 떨어져 맵 곳곳에 <b>10초</b>동안 남아있어\n";
+        infoText[4] = "주변에 가서 <b>s키</b>로 후라이팬을 휘둘러 <b>인벤토리</b>에 저장하자.\n";
+        infoText[5] = "<b>쓰레기</b>에 몸이 닿으면 재료가 모두 상해서 없어지니 조심하자.\n\n";
         infoText[6] = "이동은 <b>방향키</b>로, 점프는 <b>스페이스바</b>로 할 수 있어.\n";
-        infoText[7] = "<b>화면</b>이 자동으로 움직이고, 아래로 떨어질 수 있으니 주의하고!\n\n";
+        infoText[7] = "가끔 천적인 <b>독수리</b>가 날아다니니 피해야 돼.\n\n";
         infoText[8] = "다 모은 후 요리 <b>시간</b>에 맞춰 재료를 <b>순서대로</b> 넣으면 돼.\n";
         infoText[9] = "넣을 땐 인벤토리에서 재료를 <b>마우스</b>로 끌어서 넣자.\n\n";
         infoText[10] = "그러면 이제 요리를 시작해볼까? Let's Go!!";
---
+++ Assets/Scripts/RouletteScripts/GameDirector.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject roulette, timerText;
+    GameObject[] highLightMenu = new GameObject[3];
+
+    float rotSpeed = 0;
+    float rotAngle = 0;
+    bool isRotated = false;
+    float time = 0;
+
+    void Start()
+    {
+        this.roulette = GameObject.Find("Roulette");
+        this.highLightMenu[0] = GameObject.Find("HighlightMenu1");
+        this.highLightMenu[1] = GameObject.Find("HighlightMenu2");
+        this.highLightMenu[2] = GameObject.Find("HighlightMenu3");
+        this.timerText = GameObject.Find("TimerText");
+
+        for (int i = 0; i < highLightMenu.Length; i++)
+        {
+            this.highLightMenu[i].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !this.isRotated)
+        {
+            this.rotSpeed = 20;
+            this.rotAngle += this.rotSpeed;
+            this.roulette.transform.Rotate(0, 0, this.rotSpeed);
+        }
+
+        else
+        {
+            this.rotSpeed *= 0.99f;
+            this.rotAngle += this.rotSpeed;
+            this.roulette.transform.Rotate(0, 0, this.rotSpeed);
+
+            if (this.rotSpeed < 0.001 && this.rotSpeed != 0)
+            {
+                this.rotSpeed = 0;
+                this.isRotated = true;
+                this.rotAngle %= 360;
+
+                for (int i = 0; i < 7; i++)
+                {
+                    if ((i * 60) - 30 <= this.rotAngle && this.rotAngle <= (i * 60) + 30)
+                    {
+                        this.highLightMenu[(i % 3)].SetActive(true);
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (isRotated)
+        {
+            time += Time.deltaTime;
+            float remainTime = 4 - time;
+            this.timerText.GetComponent<Text>().text = ((int)remainTime).ToString() + "초 후 게임이 시작됩니다.";
+
+            if (remainTime <= 0)
+            {
+                this.timerText.GetComponent<Text>().text = ("0초 후 게임이 시작됩니다.");
+            }
+        }
+    }
+}
